8a61dac056fbf2f9b55bf120e5deb94022817631
dev-ashir <techashir167@gmail.com>
2026-06-22 16:35:43 +0500

    chore: add NODE_ENV and PORT environment variables to ecosystem configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,18 @@
 module.exports = {
-  apps: [
-    {
-      name: 'jetrique-website',
-      script: 'npm',
-      args: 'start',
-      cwd: '/home/repo/website',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-    }
-  ]
+    apps: [
+        {
+            name: 'jetrique-website',
+            script: 'npm',
+            args: 'start',
+            cwd: '/home/repo/website',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '1G',
+            env: {
+                NODE_ENV: 'production',
+                PORT: 3000
+            }
+        }
+    ]
 };
